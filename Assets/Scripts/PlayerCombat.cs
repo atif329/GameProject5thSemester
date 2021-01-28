36151be8c9bf3d2834d96971ecc346266165fe7b
MuhammadAhmed7862468 <63049531+MuhammadAhmed7862468@users.noreply.github.com>
2021-01-28 23:09:41 +0500

    Files Added

---
+++ Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Animator playerAnimation;
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+    public int attackDamage = 40;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0)){
+
+            Attack();
+        }
+    }
+    void Attack()
+    {
+        //Play an Attack Animation
+        playerAnimation.SetTrigger("Attack");
+
+        //Detect Enemies in Range of Attack
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        //Damage them
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+    }
+    void OnDrawGizmosSelected() 
+    {
+        if(attackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpSpeed = 7f;
+    private float movement = 0f;
+    private Rigidbody2D rigidBody;
+    public Transform groundCheckPoint;
+	public float groundCheckRadius;
+	public LayerMask groundLayer;
+	private bool isTouchingGround;
+    private Animator playerAnimation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D> ();
+        playerAnimation = GetComponent<Animator> ();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isTouchingGround = Physics2D.OverlapCircle (groundCheckPoint.position,groundCheckRadius,groundLayer);
+        movement = Input.GetAxis("Horizontal");
+        if(movement > 0f)
+		{
+			rigidBody.velocity = new Vector2 (movement * speed, rigidBody.velocity.y);
+            transform.localScale = new Vector2 (1.034049f,1.034049f);
+		}
+		else if(movement < 0f)
+		{
+			rigidBody.velocity = new Vector2 (movement * speed, rigidBody.velocity.y);
+            transform.localScale = new Vector2 (-1.034049f,1.034049f);
+		}
+        else
+		{
+			rigidBody.velocity = new Vector2 (0, rigidBody.velocity.y);
+		}
+        if(Input.GetButtonDown("Jump") && isTouchingGround){
+
+			rigidBody.velocity = new Vector2 (rigidBody.velocity.x, jumpSpeed);
+		}
+        playerAnimation.SetFloat ("Speed", Mathf.Abs(rigidBody.velocity.x));
+		playerAnimation.SetBool ("OnGround", isTouchingGround);
+    }
+}
